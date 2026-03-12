10d3906c184845ce709dce5d4c392d26a00430aa
AquilesSandoval <paulo.aquiles.sandoval@outlook.com>
2026-03-11 18:34:22 -0600

    iamges

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "assets.zyrosite.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
